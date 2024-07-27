dc9aba32869ea8dcab9bcf2fcf021dd659fb42a8
Luke Thompson <lukethompson220@gmail.com>
2024-07-28 08:50:39 +1000

    Added all the products to product list

--- src/ProductsList.ts
+++ src/ProductsList.ts
@@ -6,7 +6,10 @@ const productsList = [
     new Product("Macaron Mix of Five", "Macaron", 8.00, "macaron"),
     new Product("Classic Tiramisu", "Tiramisu", 5.50, "Tiramisu"),
     new Product("Pistachio Baklava", "Baklava", 4.00, "Baklava"),
-    new Product("Lemon Meringue Pie", "Pie", 5.00, "meringue")
+    new Product("Lemon Meringue Pie", "Pie", 5.00, "meringue"),
+    new Product("Red Velvet Cake", "Cake", 4.50, "cake"),
+    new Product("Salted Caramel Brownie", "Brownie", 5.50, "Brownie"),
+    new Product("Vanilla Panna Cotta", "Panna Cotta", 6.50, "panna-cotta")
 ]
 
 // Add this line to make the file a module
