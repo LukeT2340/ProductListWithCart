fd2b20a86d90149573f1c9026800c2eb9239516a
Luke Thompson <lukethompson220@gmail.com>
2024-07-28 10:13:15 +1000

    changed for production

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import ProductsDisplay from './components/ProductsDisplay'
 
 function App() {
   return (
-    <div className="flex flex-row bg-lightCream py-[88px]">
-      <div className='flex flex-col w-3/4 mx-[150px]'>
+    <div className="flex flex-row bg-lightCream p-[88px]">
+      <div className='flex flex-col w-[800px]'>
         <ProductsDisplay />
       </div>
-      <div className='w-1/4'>
+      <div className='w-[384px]'>
         <h1>test</h1>
       </div>
     </div>
