aeb43bfc7fd8bec1ae4a3ffdf8b14783e9bf2835
Luke Thompson <lukethompson220@gmail.com>
2024-07-28 17:25:27 +1000

    Started adding cart display

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,9 +2,14 @@ import CartProps from "../interfaces/CartProps"
 import CartItem from "./CartItem"
 
 const Cart: React.FC<CartProps>= ({ products }) => {
+    // Calculate total items in cart
+    const totalItems = products.reduce((totalItems, product) => {
+        return totalItems + product.amount
+    }, 0)
+
     return (
         <div className="font-RedHat bg-white w-[384px] rounded-[12px] text-left p-[24px]">
-            <h1 className="text-[24px] font-bold text-customRed mb-[24px]">You Cart</h1>
+            <h1 className="text-[24px] font-bold text-customRed mb-[24px]">You Cart ({totalItems})</h1>
             {products.map((product) => (
                 product.amount > 0 && (
                     <CartItem key={product.longTitle} product={product} />
