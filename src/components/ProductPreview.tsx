40b9df27d18b0fa3268a59b7500cfb2aec386970
Luke Thompson <lukethompson220@gmail.com>
2024-07-28 18:44:10 +1000

    fixed colors on product previews

--- src/components/ProductPreview.tsx
+++ src/components/ProductPreview.tsx
@@ -30,7 +30,7 @@ const ProductPreview: React.FC<ProductPreviewProps> = ({ product, isSelected, se
             <div className='relative flex justify-center items-center'>
                 <img className={`rounded-[8px] z-1 mb-[22px] ${product.amount > 0 ? 'border-2 border-customRed' : ''}`} src={`/assets/images/image-${product.imageName}-desktop.jpg`} alt={product.shortTitle} />
                 {isSelected ? (
-                    <div className='px-5 flex z-2 items-center justify-between absolute bottom-0 bg-red-600 w-[160px] rounded-full h-[44px]'>
+                    <div className='px-5 flex z-2 items-center justify-between absolute bottom-0 bg-customRed w-[160px] rounded-full h-[44px]'>
                         <button onClick={() => handleDecrementProductAmount(product)} className="flex justify-center items-center border w-[20px] h-[20px] border-white rounded-full">
                             <img src="/assets/images/icon-decrement-quantity.svg" alt="add item icon"></img>
                         </button>
@@ -40,7 +40,7 @@ const ProductPreview: React.FC<ProductPreviewProps> = ({ product, isSelected, se
                         </button>
                     </div>
                 ) : (
-                    <button onClick={() => handleIncrementProductAmount(product)} className='gap-1 flex z-2 absolute border-rose-900 border-[1px] bottom-0 bg-white w-[160px] rounded-full h-[44px] justify-center items-center'>
+                    <button onClick={() => handleIncrementProductAmount(product)} className='gap-1 flex z-2 absolute border-customRed border-[1px] bottom-0 bg-white w-[160px] rounded-full h-[44px] justify-center items-center'>
                         <img src="/assets/images/icon-add-to-cart.svg" alt="Add item to cart icon"></img>
                         <p className='font-semibold text-[14px]'>Add to Cart</p>
                     </button>
