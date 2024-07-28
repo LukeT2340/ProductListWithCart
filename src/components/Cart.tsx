4cab5dd54ec5e9960661b3b284221f58551da52f
Luke Thompson <lukethompson220@gmail.com>
2024-07-28 18:03:15 +1000

    Adjusted cart for whent he cart is empty

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -15,22 +15,31 @@ const Cart: React.FC<CartProps>= ({ products, setProducts }) => {
     return (
         <div className="flex flex-col font-RedHat bg-white w-[384px] rounded-[12px] text-left p-[24px] gap-[24px]">
             <h1 className="text-[24px] font-bold text-customRed">You Cart ({totalItems})</h1>
-            {products.map((product) => (
-                product.amount > 0 && (
-                    <CartItem key={product.longTitle} product={product} setProducts={setProducts} />
-                )
-            ))}
-            <div className="flex flex-row justify-between items-center">
-                <p className="text-[14px]">Order Total</p>
-                <p className="text-[24px] font-bold">${totalPrice.toFixed(2)}</p>
-            </div>
-            <div className="bg-lightCream flex flex-row justify-center items-center rounded-[8px] p-[16px] gap-[8px]">
-                <img src="/assets/images/icon-carbon-neutral.svg" alt="carbon neutral icon" />
-                <p className="text-[14px]">This is a <span className="font-semibold">carbon-neutral</span> delivery</p>
-            </div>
-            <button className="flex bg-customRed rounded-full text-white justify-center px-[24px] py-[16px] font-semibold text-[16px]">
-                Confirm Order
-            </button>
+            {totalItems > 0 ? (
+                <>
+                    {products.map((product) => (
+                        product.amount > 0 && (
+                            <CartItem key={product.longTitle} product={product} setProducts={setProducts} />
+                        )
+                    ))}
+                    <div className="flex flex-row justify-between items-center">
+                        <p className="text-[14px]">Order Total</p>
+                        <p className="text-[24px] font-bold">${totalPrice.toFixed(2)}</p>
+                    </div>
+                    <div className="bg-lightCream flex flex-row justify-center items-center rounded-[8px] p-[16px] gap-[8px]">
+                        <img src="/assets/images/icon-carbon-neutral.svg" alt="carbon neutral icon" />
+                        <p className="text-[14px]">This is a <span className="font-semibold">carbon-neutral</span> delivery</p>
+                    </div>
+                    <button className="flex bg-customRed rounded-full text-white justify-center px-[24px] py-[16px] font-semibold text-[16px]">
+                        Confirm Order
+                    </button>
+                </>
+            ) : (
+                <div className="flex flex-col items-center">
+                    <img src="/assets/images/illustration-empty-cart.svg" />
+                    <p className="text-[14px] text-lightBrown font-semibold">Your added items appear here</p>
+                </div>
+            )}
         </div>
     )
 }
