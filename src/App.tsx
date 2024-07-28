2e261b714fcd091eeaa871ea90b3aa33f5956b39
Luke Thompson <lukethompson220@gmail.com>
2024-07-28 18:17:54 +1000

    Added certain degree of responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const [products, setProducts] = useState(productsList);
 
   return (
-    <div className="flex lg:flex-row flex-col items-center lg:items-start justify-center bg-lightCream lg:p-[88px] gap-[32px] md:p-[40px] lg:p-[88px]">
+    <div className="flex lg:flex-row flex-col items-center lg:items-start justify-center bg-lightCream lg:p-[88px] gap-[32px] p-[24px] md:p-[40px] lg:p-[88px]">
       <ProductsDisplay products={products} setProducts={setProducts} />
       <Cart products={products} setProducts={setProducts} />
     </div>
